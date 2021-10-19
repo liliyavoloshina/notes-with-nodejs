bcd006d9e1ea582d411e754dd4ea26d82cd91c7f
Liliya <liliyavoloshine@gmail.com>
2021-10-19 16:59:26 +0400

    feat: rewrite with class

--- index.js
+++ index.js
@@ -6,86 +6,92 @@ const [command, title, content] = process.argv.slice(2)
 const folderName = './notes',
   fileName = 'notes'
 
-init()
+  class NoteBook {
+    constructor() {
+      this.init()
+    }
+  
+    init() {
+      if (!fs.existsSync(folderName)) {
+        fs.mkdir(path.join(__dirname, 'notes'), e => {
+          if (e) stdout.write('Something went wrong while creating folder...')
+        })
+      }
+  
+      if (!fs.existsSync(fileName)) {
+        const emptyNotes = JSON.stringify([])
+        fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), emptyNotes, e => {
+          if (e) stdout.write('Something went wrong while creating file...')
+        })
+      }
+    }
+  
+    create(title, content) {
+      fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
+        if (e) stdout.write('Something went wrong while reading file...')
+        const oldData = JSON.parse(data)
+        oldData.push({ title, content })
+        const updatedData = JSON.stringify(oldData)
+  
+        fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), updatedData, e => {
+          if (e) stdout.write('Something went wrong while writing file...')
+        })
+      })
+    }
+  
+    list() {
+      fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
+        if (e) stdout.write('Something went wrong while reading file...')
+        const arr = JSON.parse(data)
+        arr.forEach((note, idx) => {
+          console.log(`Note ${idx + 1}: ${note.title}`)
+        })
+      })
+    }
+  
+    view(title) {
+      fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
+        if (e) stdout.write('Something went wrong while reading file...')
+        const arr = JSON.parse(data)
+        const foundNote = arr.find(note => note.title === title)
+  
+        if (!foundNote) {
+          stdout.write('There is no note with this title...')
+        } else {
+          stdout.write(`${foundNote.content}`)
+        }
+      })
+    }
+  
+    remove(title) {
+      fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
+        if (e) stdout.write('Something went wrong while reading file...')
+        const arr = JSON.parse(data)
+        const filteredData = arr.filter(note => note.title !== title)
+        const dataToUpdate = JSON.stringify(filteredData)
+  
+        fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), dataToUpdate, e => {
+          if (e) stdout.write('Something went wrong while writing file...')
+        })
+      })
+    }
+  }
+
+const noteBook = new NoteBook()
 
 switch (command) {
   case 'create':
-    create(title, content)
+    noteBook.create(title, content)
     break
   case 'list':
-    list()
+    noteBook.list()
     break
   case 'remove':
-    remove(title)
+    noteBook.remove(title)
     break
   case 'view':
-    view(title)
+    noteBook.view(title)
     break
   default:
     stdout.write('Valid command: create, list, remove')
-}
-
-function init() {
-  if (!fs.existsSync(folderName)) {
-    fs.mkdir(path.join(__dirname, 'notes'), e => {
-      if (e) stdout.write('Something went wrong while creating folder...')
-    })
-  }
-
-  if (!fs.existsSync(fileName)) {
-    const emptyNotes = JSON.stringify([])
-    fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), emptyNotes, e => {
-      if (e) stdout.write('Something went wrong while creating file...')
-    })
-  }
-}
-
-function create(title, content) {
-  fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
-    if (e) stdout.write('Something went wrong while reading file...')
-    const oldData = JSON.parse(data)
-    oldData.push({ title, content })
-    const updatedData = JSON.stringify(oldData)
-
-    fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), updatedData, e => {
-      if (e) stdout.write('Something went wrong while writing file...')
-    })
-  })
-}
-
-function list() {
-  fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
-    if (e) stdout.write('Something went wrong while reading file...')
-    const arr = JSON.parse(data)
-    arr.forEach((note, idx) => {
-      console.log(`Note ${idx + 1}: ${note.title}`)
-    })
-  })
-}
-
-function view(title) {
-  fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
-    if (e) stdout.write('Something went wrong while reading file...')
-    const arr = JSON.parse(data)
-    const foundNote = arr.find(note => note.title === title)
-
-    if (!foundNote) {
-      stdout.write('There is no note with this title...')
-    } else {
-      stdout.write(`${foundNote.content}`)
-    }
-  })
-}
-
-function remove(title) {
-  fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
-    if (e) stdout.write('Something went wrong while reading file...')
-    const arr = JSON.parse(data)
-    const filteredData = arr.filter(note => note.title !== title)
-    const dataToUpdate = JSON.stringify(filteredData)
-
-    fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), dataToUpdate, (e) => {
-      if (e) stdout.write('Something went wrong while writing file...')
-    })
-  })
 }
