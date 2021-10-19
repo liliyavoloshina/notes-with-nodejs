0ef0aefed24298046e3cc584c73dadb2fc022c88
Liliya <liliyavoloshine@gmail.com>
2021-10-19 16:37:29 +0400

    feat: implement list fn

--- index.js
+++ index.js
@@ -24,28 +24,38 @@ switch (command) {
 
 function init() {
   if (!fs.existsSync(folderName)) {
-    fs.mkdir(path.join(__dirname, 'notes'), err => {
-      if (err) stdout.write('Something went wrong while creating folder...')
+    fs.mkdir(path.join(__dirname, 'notes'), e => {
+      if (e) stdout.write('Something went wrong while creating folder...')
     })
   }
 
   if (!fs.existsSync(fileName)) {
     const emptyNotes = JSON.stringify([])
-    fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), emptyNotes, err => {
-      if (err) stdout.write('Something went wrong while creating file...')
+    fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), emptyNotes, e => {
+      if (e) stdout.write('Something went wrong while creating file...')
     })
   }
 }
 
 function create(title, content) {
-  fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (err, data) => {
-    if (err) stdout.write('Something went wrong while reading file...')
+  fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
+    if (e) stdout.write('Something went wrong while reading file...')
     const oldData = JSON.parse(data)
     oldData.push({ title, content })
     const updatedData = JSON.stringify(oldData)
 
-    fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), updatedData, err => {
-      if (err) stdout.write('Something went wrong while writing file...')
+    fs.writeFile(path.join(__dirname, 'notes', 'notes.json'), updatedData, e => {
+      if (e) stdout.write('Something went wrong while writing file...')
+    })
+  })
+}
+
+function list() {
+  fs.readFile(path.join(__dirname, 'notes', 'notes.json'), (e, data) => {
+    if (e) stdout.write('Something went wrong while reading file...')
+    const arr = JSON.parse(data)
+    arr.forEach((note, idx) => {
+      console.log(`Note ${idx + 1}: ${note.title}`)
     })
   })
 }
